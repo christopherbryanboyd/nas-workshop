5495029c8fd9d02f7a3747fbd071232ea8ae1a71
Gregory Amerson <gregory.amerson@liferay.com>
2017-10-10 13:28:24 -0500

    buildService

---
+++ modules/todo/todo-service/src/main/resources/META-INF/sql/tables.sql
@@ -0,0 +1,13 @@
+create table TODO_Todo (
+	uuid_ VARCHAR(75) null,
+	todoId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	title VARCHAR(75) null,
+	description VARCHAR(75) null,
+	completed BOOLEAN
+);
