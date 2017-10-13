f5c49f7302a28e8230e0c1793e7ca57409cc6efe
Christopher Bryan Boyd <christopher.boyd@liferay.com>
2017-10-12 21:50:48 -0500

    Add Mocks and such

--- modules/todo/todo-service/src/test/java/com/liferay/todo/impl/test/MyServiceImplMockTest.java
+++ modules/todo/todo-service/src/test/java/com/liferay/todo/impl/test/MyServiceImplMockTest.java
@@ -1,23 +1,89 @@
 package com.liferay.todo.impl.test;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.reflect.Whitebox;
+
+import static org.easymock.EasyMock.*;
+import static org.junit.Assert.*;
+import org.easymock.IAnswer;
 
 import com.liferay.todo.impl.MyServiceImpl;
 import com.liferay.todo.model.Todo;
+import com.liferay.todo.service.TodoLocalService;
 
 public class MyServiceImplMockTest {
 
 
-//	@Test
+	@Before
+	public void setUp() {
+		mockTodo();
+		
+		mockTodoLocalService();
+		
+		mockMyServiceImpl();
+	}
+	
+	private void mockTodo() 
+	{
+		todo = mock(Todo.class);
+		
+		todo.setCompleted(Boolean.TRUE);
+		
+		expectLastCall().andVoid().once();
+		
+		expect(todo.getCompleted()).andReturn(Boolean.TRUE).once();
+		
+		replay(todo);
+	}
+	
+	private void mockTodoLocalService()
+	{
+		todoLocalService = mock(TodoLocalService.class);
+		
+		expect(todoLocalService.updateTodo(isA(Todo.class))).andAnswer(new IAnswer<Todo>() {
+
+			@Override
+			public Todo answer() throws Throwable {
+				Todo todo = (Todo) getCurrentArguments()[0];
+				
+				assertTrue(todo.getCompleted());
+				
+				return todo;
+
+			}
+		}).once();
+		
+		replay(todoLocalService);
+	}
+	
+	private void mockMyServiceImpl() 
+	{
+		myServiceImpl = mock(MyServiceImpl.class);
+		
+		Whitebox.setInternalState(myServiceImpl, "todoService", todoLocalService);
+
+		myServiceImpl.stillNeedsWork(isA(Todo.class));
+		
+		expectLastCall().andVoid().once();
+		
+		replay(myServiceImpl);
+	}
+	
+	@Test
 	public void myServiceImplTest() throws Exception {
-		MyServiceImpl myServiceImpl = new MyServiceImpl();
 
 		myServiceImpl.stillNeedsWork(todo);
-
-		Assert.assertFalse(todo.getCompleted());
+		
+		PowerMock.verifyAll();
 	}
+	
+	private TodoLocalService todoLocalService = null;
+	
+	private MyServiceImpl myServiceImpl = null;
 
-
-	Todo todo = null;
+	private Todo todo = null;
 
 }
